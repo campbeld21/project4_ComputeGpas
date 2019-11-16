f238e26aef222ce76802b908aa3365aa6a0dd923
David Campbell <campbeld21@up.edu>
2019-11-15 20:38:23 -0800

    changes

--- computeGpas.py
+++ computeGpas.py
@@ -2,19 +2,13 @@
 import os
 import sys
 
-def main():
-	filepath = "input.txt"
-	f = open(filepath, "r")
-	lines = [] 
-	lines = f.readlines()
-	
-	for line in lines:
-		line.split(' ')
-		print(line)
 
-	f.close()
-	#print(lines)
-	return
+#filepath = "input.txt"
+#f = open(filepath, "r")
+lines = [] 
 
-if __name__ == "__main__":
-	main()
+for line in sys.stdin:
+	separatedValues = line.split(' ')
+	lines.append(separatedValues)
+
+print(lines)
