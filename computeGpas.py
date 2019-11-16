9fcec0c741fb53ae318a290f9acfb6f2b0b10bf1
campbeld21 <campbeld21@up.edu>
2019-11-15 20:06:03 -0800

    changes

--- computeGpas.py
+++ computeGpas.py
@@ -1,1 +1,13 @@
 #David Campbell 
+import os
+import sys
+
+lines = [] 
+#f = open('input.txt', 'r')
+
+for line in sys.stdin:
+    #print(line)
+    separatedValues = line.split()
+    lines.append(separatedValues)
+
+print(lines)
