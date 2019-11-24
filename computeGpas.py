4cb1153dcb4950a56ceaf2e66d111f17b7780961
Olivia <ofigueira@scu.edu>
2019-11-23 22:02:10 -0800

    done :)

--- computeGpas.py
+++ computeGpas.py
@@ -1,5 +1,7 @@
 # David Campbell
+
 import os
+import re
 import sys
 import pprint
 from collections import OrderedDict
@@ -12,7 +14,7 @@ def calculateGPA(letter, gpa, credit):
 	elif letter == 'A-':
 		gpa_temp = (4.0-0.33)
 	elif letter == 'A+':
-		gpa_temp = (4.0+0.33)
+		gpa_temp = (4.0)
 	elif letter == 'B':
 		gpa_temp = 3.0
 	elif letter == 'B-':
@@ -34,23 +36,100 @@ def calculateGPA(letter, gpa, credit):
 	elif letter == 'F':
 		gpa_temp = 0.0
 	else:
-		print("Has an incorrect Input")
 		return("ignore") #if no grade in between A-F, ignore this parameter 
 		
 	return(gpa + (gpa_temp * credit))
 
 def sumCredits(inCredit, sumCredits):
+	# check if invalid credits
+	if inCredit < 0:
+		return(-1) #error return value
     	# sum up credits
 	sumCredits += inCredit
 	return(sumCredits)
 
+def processLine(index, line, gradebook, fullName, flag):
+
+	## COURSE PROCESSING
+	#check validity of course name and number
+	if (re.match('^[A-Z]+$', line[0])) == False or (line[1].isdigit() == False):
+		print("Error: Course name invalid at line {}.\n".format(index))
+		return(-1,-1,-1)
+	# check if course was already added 
+	course = line[0] + line[1] # 'CS273' is course string
+	if flag == 1:
+		# new student,  no course processing necessary, but if 1, need processing
+		if course in gradebook[fullName]["classes"]:
+			print("Error: {} was enrolled in the course {} more than once. Error in line {}.\n".format(fullName, course, index))
+			return(-1,-1,-1)
+	#otherwise add course to classes list in dictionary
+	# return course
+
+	## GPA PROCESSING
+	grade = line[6].strip('\n')
+	if flag == 1:
+		retGPA = calculateGPA(grade, gradebook[fullName]["gpa"], int(line[3]))
+	elif flag == 0:
+		retGPA = calculateGPA(grade, 0.0, int(line[3]))
+	if (retGPA == "ignore"):
+		print("Error: Invalid grade at line {}.\n".format(index))
+		return(-1,-1,-1)
+	#return retGPA
+
+	## CREDIT PROCESSING
+	if flag == 1:
+		retCredit = sumCredits(int(line[3]), gradebook[fullName]["credits"])
+	elif flag == 0:
+		retCredit = sumCredits(int(line[3]), 0)
+	if retCredit == -1:
+		#invalid credit entry in line, print line for error messasge
+		print("Error: Invalid number of credits in line {}\n".format(index))
+		return(-1,-1,-1)
+	#otherwise update credit number in gradebook
+	#return retCredit
+	return(course, retGPA, retCredit)
+
+def processCourses(lines):
+	# print error message if a course is listed with different credits
+	courses = {}
+	for index,line in enumerate(lines):
+		## LINE ENTRIES
+		if line[0] == '\n':
+			continue
+		# if wrong number of entries on line skip (must be 7)
+		if len(line) != 7 :
+			#print("Error: Invalid number of items on line {}.\n".format(index))
+			continue
+		flag = 0
+		for key,value in courses.items():
+			if key == (line[0]+line[1]):
+				flag = 1
+				if courses[key] != line[3]:
+					print("Error: Course {} listed with differing number of credits on line {}.\n".format(key, index))
+		if flag == 0:
+			# add course and credits to dict
+			new = {(line[0]+line[1]): line[3]}
+			courses.update(new)
 def main():
 	lines = [] 
 	for line in sys.stdin:
+		# if empty line, skips that line
 		separatedValues = line.split(' ')
 		lines.append(separatedValues)
+
+	## CHECK COURSE AND CREDIT COMBO
+	processCourses(lines)
+
 	gradebook = {} #list of student dictionaries
-	for line in lines:
+	for index,line in enumerate(lines):
+		## LINE ENTRIES
+		if line[0] == '\n':
+			continue
+		# if wrong number of entries on line skip (must be 7)
+		if len(line) != 7 :
+			print("Error: Invalid number of items on line {}.\n".format(index))
+			continue
+
 		# save first and last name in format of expect.txt
 		fullName = line[5]+', '+line[4] #check this if there is an extra line at the bottom 
 		flag = 0
@@ -58,38 +137,37 @@ def main():
 		for key,value in gradebook.items():
 			if key == fullName:
 				flag = 1 #found name
-				# update GPA
-				grade = line[6].strip('\n')
-				ret = calculateGPA(grade, gradebook[key]["gpa"], int(line[3]))
-				if (ret == "ignore"):
+				#def processLine(index, line, gradebook, key, fullName):
+				course, retGPA, retCredit = processLine(index, line, gradebook, fullName, flag)
+				if course == -1:
 					continue
-				else:
-					gradebook[key]["gpa"] = ret	
-				# update credits 
-				gradebook[key]["credits"] = sumCredits(int(line[3]), gradebook[key]["credits"])
+				# else
+				gradebook[key]["classes"].append(course)
+				gradebook[key]["gpa"] = retGPA
+				gradebook[key]["credits"] = retCredit
 		if flag == 0:
-			grade = line[6].strip() # strip method helps deal with the new line values in input file
-			#student not in gradebook yet, insert with new values
-			ret_gpa = calculateGPA(grade, 0.0, int(line[3]))
-			if ret_gpa == "ignore":
-                        	continue
-                        # update credits 
-			ret_cred = sumCredits(int(line[3]), 0)
+			#def processLine(index, line, gradebook, key, fullName):
+			course, retGPA, retCredit = processLine(index, line, gradebook, fullName, flag)
+			if course == -1:
+				continue
 			student = {
 				fullName:{
 					"name": fullName,
-					"gpa": ret_gpa,
-					"credits": ret_cred,
-					"nameLength": 0
+					"gpa": retGPA,
+					"credits": retCredit,
+					"nameLength": 0,
+					"classes": [course]
 				}
 			}
 			gradebook.update(student) #insert new student into gradebook
 			
-	#pprint.pprint(gradebook)
 	for key,value in gradebook.items():
 		# traverse dictionary and finish GPA calculation
-		gradebook[key]["gpa"] /= gradebook[key]["credits"]
-		gradebook[key]["gpa"] = format(gradebook[key]["gpa"], '.2f')
+		if gradebook[key]["credits"] == 0:
+			gradebook[key]["gpa"] = '----'
+		else:
+			gradebook[key]["gpa"] /= gradebook[key]["credits"]
+			gradebook[key]["gpa"] = format(gradebook[key]["gpa"], '.2f')
 
 	maxLength = 0 
 	for key in gradebook:
@@ -106,13 +184,13 @@ def main():
 		diff_str = ' ' * difference
 		namePrint = diff_str + gradebook[key]["name"]
 		gradebook[key]["name"] = namePrint #save the way we want to print it in dictionary
-		#print(namePrint, gradebook[key]["gpa"], gradebook[key]["credits"])
 
 	ordered = OrderedDict(sorted(gradebook.items(), key = lambda x: getitem(x[1], "gpa"), reverse=True))
-
-
-	for key in ordered:
-		print(ordered[key]["name"], ordered[key]["gpa"], ordered[key]["credits"])
+	if gradebook == {}:
+		print("Error: No valid entries in input.\n")
+	else:
+		for key in ordered:
+			print(ordered[key]["name"], ordered[key]["gpa"], ordered[key]["credits"])
 
 if __name__ == '__main__':
 	main()
